d8ca6f1adfaa39fe121f6b00bb870dbc2f11a51e
Marti Regola <marti@pop-os.localdomain>
2020-07-26 17:48:25 +0200

    domain tests

---
+++ tests/fixtures/dictionary_collection.py
@@ -0,0 +1,11 @@
+from copy import deepcopy
+
+test_work = {
+    "title": "Bmat Remix ft. gravity",
+    "contributors": ["Martí Regola", "Isaac Newton"],
+    "iswc": "123123",
+    "source": "mozilla",
+    "id": 1,
+}
+
+expected_work = deepcopy(test_work)
---
+++ tests/unit/test_domain_works.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from app.business_layers.domain import Work
+from tests.fixtures.dictionary_collection import test_work, expected_work
+
+
+class TestWorkDomain(TestCase):
+    def test_work_init(self):
+        work = Work(**test_work)
+
+        self.assertEqual(work.to_dict(), expected_work)
+
+    def test_work_from_dict(self):
+        work = Work.from_dict(test_work)
+        self.assertAlmostEqual(
+            work.to_dict().get("iswc"), expected_work.get("iswc")
+        )
+
+    def test_work_to_dict(self):
+        work = Work.from_dict(test_work)
+        self.assertEqual(work.to_dict(), expected_work)
+
+    def test_equality(self):
+        work1 = Work.from_dict(test_work)
+        work2 = Work.from_dict(test_work)
+        self.assertEqual(work1, work2)
+        self.assertEqual(work1, test_work)
+        self.assertEqual(test_work, work1)
+        self.assertTrue(work1 in [test_work])
